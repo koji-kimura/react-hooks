cf20f4b7f359fd6eec38c44a32ffd9b1577bb8ec
k-kimura <kimura_511@me.com>
2020-04-11 19:49:39 +0900

    setup react context provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers';
 
 import Events from '../components/Events';
 import EventForm from '../components/EventForm';
 
+import AppContext from '../contexts/AppContext';
+
+import reducer from '../reducers';
+
+console.log({ AppContext });
+
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value="this is contextMessage">
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
